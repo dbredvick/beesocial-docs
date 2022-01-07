96c3c8ae085c1b6368f76a713f2061f920672478
Drew Bredvick <dbredvick@gmail.com>
2022-01-06 22:31:54 -0600

    fix: more clear uninstall

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.js",
-  // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
+  defaultMenuCollapsed: false,
+  unstable_staticImage: true, // to enable Nextra's auto image import
+  floatTOC: false,
 });
 module.exports = withNextra({
   reactStrictMode: true,
